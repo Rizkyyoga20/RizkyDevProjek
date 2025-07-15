0d155c6a8f5f93d90b22faeee3919b70af9d7e28
Rizkyyga20 <rizkyyog281@gmail.com>
2025-07-15 10:21:14 +0700

    perbaikan file gambar

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  assetsInclude: ['**/*.{png,PNG}'],
+  assetsInclude: ['**/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF,svg,SVG}'],
   plugins: [
     vue(),
     //vueDevTools(),
