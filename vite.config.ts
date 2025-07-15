f30d57bd17f9eb5b5bbcb20d64666a17ac23379f
Rizkyyga20 <rizkyyog281@gmail.com>
2025-07-15 19:58:54 +0700

    perbaikan root setup vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  root: './src',
   assetsInclude: ['**/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF,svg,SVG}'],
   plugins: [
     vue(),
