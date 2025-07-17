5625448b6a1167114bfc0e883d90b08e456df76d
Rizkyyga20 <rizkyyog281@gmail.com>
2025-07-17 10:07:22 +0700

    perbaikan setup vercel hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vercel from 'vite-plugin-vercel'
 //import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 
@@ -7,6 +8,7 @@ export default defineConfig({
   assetsInclude: ['**/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF,svg,SVG}'],
   plugins: [
     vue(),
+    vercel(),
     //vueDevTools(),
   ],
 })
